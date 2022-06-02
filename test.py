4eb271764cfcd37c799b4037d525bd0547bf8332
Mridul Viralka <f20200479@pilani.bits-pilani.ac.in>
2022-06-02 17:56:15 +0530

    opened videos

--- test.py
+++ test.py
@@ -1,7 +1,21 @@
 import cv2 as cv
 
-img = cv.imread('Images\girl.jpg')
+# img = cv.imread('Images\\deer.jpg')
 
-cv.imshow("girl", img)
+# cv.imshow("girl", img)
+
+# cv.waitKey(0)
+
+capture = cv.VideoCapture('Videos\Lambs eating grass.mp4')
+
+while True:
+    istrue , frame = capture.read()
+    cv.imshow('lambs', frame)   
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
 
 cv.waitKey(0)
