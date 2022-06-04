21c0b599445f67dba11fea0d2f09ff1da683cfdf
Mridul Viralka <f20200479@pilani.bits-pilani.ac.in>
2022-06-04 10:17:15 +0530

    facedetiction

--- OpenCv/project_face_recognition/facedetection.py
+++ OpenCv/project_face_recognition/facedetection.py
@@ -1,1 +1,24 @@
-print("hi")
+import cv2 as cv
+import image_resized as ir
+
+
+img_list =ir.allimg(r"C:\Users\mridu\Desktop\agnext\Agnext\OpenCv\Images")
+
+
+for img in img_list:
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # cv.imshow('gray',gray)
+
+    haar_cascade = cv.CascadeClassifier('project_face_recognition\haar_face.xml')
+
+    face_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+
+    print(f"Number of faces detected: {len(face_rect)}")
+
+    for (x,y,w,h) in face_rect:
+        cv.rectangle(img, (x,y), (x+w, y+h), (0,255,0), 2)
+
+    cv.imshow("dected faces", img)
+    cv.waitKey(2200)
+
+
