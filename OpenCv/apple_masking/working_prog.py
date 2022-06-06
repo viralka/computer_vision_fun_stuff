d18f3e77ad52d48caead28ac14b7909dcfee7591
Mridul Viralka <f20200479@pilani.bits-pilani.ac.in>
2022-06-06 10:04:29 +0530

    New tool for hsv finding

--- OpenCv/apple_masking/working_prog.py
+++ OpenCv/apple_masking/working_prog.py
@@ -4,12 +4,35 @@ import image_opener as ip
 
 # opening all the images from a folder
 img_list = ip.allimg(r'apple_masking\apple_image')
-img = img_list[5]
-# cv.imshow( 'im`',img)
+img = img_list[6]
+cv.imshow( 'im`',img)
+
+# taking the image 
+gray  = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# threashoalding for total area
+threshold , thres = cv.threshold(gray, 10, 225, cv.THRESH_BINARY)
+# cv.imshow("threshold", thres)
+# area_T = cv.countNonZero(thres)
+
+#finding conture of it 
+contours, hierarchy = cv.findContours(thres,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+cv.drawContours(img, contours, -1, (0, 255, 255), 1)
+
+
+# # now finding area in the contours
+
+area_T = cv.contourArea(contours[0])
+print(area_T,"total area")
+
+# now masking image for less error
+img = cv.bitwise_and(img, img , mask= thres)
+cv.imshow("img",img)
 
 # converitn all the images in hsv format and bluring a litte
 hsv_og = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 hsv = cv.GaussianBlur(hsv_og,(5,5),0)
+cv.imshow("img",img)
 
 # defining color values
 
@@ -23,11 +46,18 @@ mask = cv.inRange(hsv, lower_r, upper_r)
 
 # finding the conture 
 contours, hierarchy = cv.findContours(mask,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-cv.drawContours(img, contours, -1, (0, 255, 0), 3)
+cv.drawContours(img, contours, -1, (0, 255, 0), 1)
 cv.imshow("img",img)
 
 # now finding area in the contours
 area = cv.contourArea(contours[0])
-print(area)
+print(area, "of red")
 
-cv.waitKey(2200)
+if area_T != 0 and area_T > area:
+    percent= (area/area_T)*100
+    print("percent of red area is = ", percent) 
+
+else : 
+    print("There is a error")
+
+cv.waitKey(00)
---
+++ OpenCv/toos/hsv_color_finder.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+
+def callback(x):
+    pass
+
+cap = cv2.VideoCapture(0)
+# img = cv2.imread('IMG_20201224_12304.jpg')
+
+
+#img = cv2.imread('wheat-upper1.png')
+img = cv2.imread(r"C:\Users\mridu\Desktop\agnext\Agnext\OpenCv\apple_masking\apple_image\a1.jpg")
+
+new_rows = int(img.shape[0]/2)
+new_cols = int(img.shape[1]/2)
+img = cv2.resize(img, (new_cols,new_rows), fx=0.5, fy=0.5)
+
+cv2.namedWindow('Colorbars')
+
+ilowH = 0
+ihighH = 179
+
+ilowS = 0
+ihighS = 255
+ilowV = 0
+ihighV = 255
+
+# create trackbars for color change
+cv2.createTrackbar('lowH','Colorbars',ilowH,179,callback)
+cv2.createTrackbar('highH','Colorbars',ihighH,179,callback)
+
+cv2.createTrackbar('lowS','Colorbars',ilowS,255,callback)
+cv2.createTrackbar('highS','Colorbars',ihighS,255,callback)
+
+cv2.createTrackbar('lowV','Colorbars',ilowV,255,callback)
+cv2.createTrackbar('highV','Colorbars',ihighV,255,callback)
+
+
+
+img2 = np.copy(img)
+hsv = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+while(1):
+    # ret, frame = cap.read()
+    ilowH = cv2.getTrackbarPos("lowH", "Colorbars")
+    ihighH = cv2.getTrackbarPos("highH", "Colorbars")
+    ilowS = cv2.getTrackbarPos("lowS", "Colorbars")
+    ihighS = cv2.getTrackbarPos("highS", "Colorbars")
+    ilowV = cv2.getTrackbarPos("lowV", "Colorbars")
+    ihighV = cv2.getTrackbarPos("highV", "Colorbars")
+
+    
+    
+    lower_hsv = np.array([ilowH, ilowS, ilowV])
+    higher_hsv = np.array([ihighH, ihighS, ihighV])
+    mask = cv2.inRange(hsv, lower_hsv, higher_hsv)
+    cv2.imshow('mask', mask)
+
+    img2 = np.copy(img)
+    img2[mask == 0] = 0
+
+    hsv2 = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+
+    gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+#    hsv2[:,:,2] = gray
+
+    
+
+    cv2.imshow('frame', img2)
+#    cv2.imshow('hsv', hsv2)
+
+#    cv2.imshow('gray', gray)
+    # print(ilowH, ihighH,  ilowS, ihighS,  ilowV, ihighV )
+
+    
+    if(cv2.waitKey(1) & 0xFF == 27):# ord('q')):
+        break
+
+
+cv2.destroyAllWindows()
+
+
+
