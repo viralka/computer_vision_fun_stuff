5824325d8cf6b6be0ea4a4715c58de7b4ec3b532
Mridul Viralka <f20200479@pilani.bits-pilani.ac.in>
2022-06-06 17:07:08 +0530

    implimente the morning script in live video

---
+++ OpenCv/SP_capturing_object_in_videos/naive_object_recognition/onionrecognition.py
@@ -0,0 +1,71 @@
+import cv2 as cv
+import numpy as np
+
+url = r"https://192.168.117.63:8080/video"
+cap = cv.VideoCapture(url)
+while(True):
+    camera, frame = cap.read()
+    if frame is not None:
+        img = frame
+        # cv.imshow( 'im`',img)
+        # converitn all the images in hsv format and bluring a litte
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        # hsv = cv.GaussianBlur(hsv_og,(11,11),0)
+        # cv.imshow("img",img)
+
+        # defining color values
+
+        # Red
+        lower = np.array([25,0,0])
+        upper = np.array([144, 255, 255])
+
+        # createing a mask 
+        mask = cv.inRange(hsv, lower, upper)
+        mask = cv.bitwise_not(mask)
+        img = cv.bitwise_and(img , img, mask= mask)
+        img1 = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        lot_blur = cv.GaussianBlur(img1, (11,11),0)
+        # cv.imshow("mask", lot_blur)
+
+
+        cont, her = cv.findContours(lot_blur, cv.RECURS_FILTER, cv.CHAIN_APPROX_SIMPLE)
+
+        i = 0
+        for cont in cont:
+            area = cv.contourArea(cont)
+            if area > 1000:
+                # cv.drawContours(img, cont, -1, (0, 255, 0), 1)
+                x,y,h,w = cv.boundingRect(cont)
+                cropped_img = img[y:y+w, x:x+h]
+                # cv.imshow("apple_obj",cropped_img)
+                # name = "apple_obj" + str(i) + ".jpg"
+                # cv.imwrite(name, cropped_img)
+                img = cv.rectangle(frame, (x,y), (x+h, y+w), (0,255,0), 2)
+                i += 1
+            cv.imshow("Frame", img)
+    q = cv.waitKey(10)
+    if q==ord("q"):
+        break
+cv.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ OpenCv/toos/image_opener.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+from matplotlib import image
+import numpy as np
+import os
+
+
+
+
+# resizing images but also saving it ratio
+def resizeimg(img, size, interpolation = cv.INTER_CUBIC):
+  h, w = img.shape[:2]
+  c = None if len(img.shape) < 3 else img.shape[2]
+  if h == w: return cv.resize(img, (size, size), interpolation)
+  if h > w: dif = h
+  else:     dif = w
+  x_pos = int((dif - w)/2.)
+  y_pos = int((dif - h)/2.)
+  if c is None:
+    mask = np.zeros((dif, dif), dtype=img.dtype)
+    mask[y_pos:y_pos+h, x_pos:x_pos+w] = img[:h, :w]
+  else:
+    mask = np.zeros((dif, dif, c), dtype=img.dtype)
+    mask[y_pos:y_pos+h, x_pos:x_pos+w, :] = img[:h, :w, :]
+  return cv.resize(mask, (size, size), interpolation)
+
+
+#open all the images in a folder
+def allimg(folder):
+    image =load_images_from_folder(folder)
+    imlist=[]
+    i = 0
+    for img in image:
+        name = "img" + str(i)
+        imlist.append(resizeimg(img, 500))
+    
+    return imlist
+    
+
+
+# load_images_from_folder
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+
