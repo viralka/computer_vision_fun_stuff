f1cf519ff561dbe6a94a808568fa242c97f22c30
Mridul Viralka <f20200479@pilani.bits-pilani.ac.in>
2022-06-06 23:16:13 +0530

    basic hand tracking program

--- OpenCv/SP_capturing_object_in_videos/HandTracking/HandTracking.py
+++ OpenCv/SP_capturing_object_in_videos/HandTracking/HandTracking.py
@@ -3,28 +3,50 @@ import mediapipe as mp
 import time 
 import image_opener as io
 
-
-url = r"https://192.168.29.61:8080/video"
+# load the video from the phone 
+url = r"https://192.168.29.61:8080/video" # change everythime we have to jump ip
 cap = cv.VideoCapture(url)
 
+# using inbuild methord
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+# for frame per second
+pTime = 0
+cTime = 0
+
 while(True):
+
+    # reading the frame from the camera
     camera, frame = cap.read()
     frame = io.resizeimg(frame, 1000)
 
     if frame is not None:
         imgRgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
         result = hands.process(imgRgb)
-        print(result.multi_hand_landmarks)
+        # print(result.multi_hand_landmarks)
         if result.multi_hand_landmarks:
             for handlms in result.multi_hand_landmarks:
+                for id, lm in enumerate(handlms.landmark):
+                    
+                    # print(id,lm)
+                    h, w,c = frame.shape
+                    cx , cy = int(lm.x*w), int(lm.y*h)
+                    print(id,cx,cy)
+
+                    if id == 4: # each id is equal some specific point in the hand
+                        cv.circle(frame, (cx,cy),25,(255,0,255), cv.FILLED)
+
                 mpDraw.draw_landmarks(frame,handlms, mpHands.HAND_CONNECTIONS)
 
+        cTime = time.time()
+        fps  = 1/(cTime-pTime)
+        pTime = cTime 
+
+        cv.putText(frame, str(int(fps)), (10,70), cv.FONT_HERSHEY_COMPLEX,3,(255,0,255),3)
         cv.imshow("Frame", frame)
-    q = cv.waitKey(5)
+    q = cv.waitKey(1) 
     if q==ord("q"):
         break
 
